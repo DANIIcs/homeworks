51a24b0e795820ed506352d8b21d84b6bfa6fde7
Daniel <humbertodanielcaballeros@gmail.com>
2024-11-10 20:46:01 -0500

    revisar

--- hw2/hw2_impl.h
+++ hw2/hw2_impl.h
@@ -17,37 +17,73 @@ Heap<T>::Heap(bool isMinHeap) : minHeap(isMinHeap) {
 template <typename T>
 void Heap<T>::insert(T const& element) {
   data.push_back(element);
+  int index = data.size()-1;
+  while (index > 0) {
+    int parent = (index - 1) / 2;
+
+    if ((minHeap && data[index] < data[parent]) || (!minHeap && data[index] > data[parent])) {
+      std::swap(data[index], data[parent]);
+      index = parent; 
+    } else {
+      break;
+    }
+  }
   
 }
 
 template <typename T>
 T Heap<T>::extractTop() {
-  // COMPLETE HERE
-  return T{};
+  if (isEmpty()) return T{};
+
+  T top = data[0];
+  data[0] = data.back();
+  data.pop_back();
+
+  int index = 0;
+  int size = data.size();
+
+  while (true) {
+    int leftChild = 2 * index + 1;
+    int rightChild = 2 * index + 2;
+    int swapIndex = index;
+
+    if (leftChild < size && ((minHeap && data[leftChild] < data[swapIndex]) || (!minHeap && data[leftChild] > data[swapIndex]))) {
+      swapIndex = leftChild;
+    }
+    if (rightChild < size && ((minHeap && data[rightChild] < data[swapIndex]) || (!minHeap && data[rightChild] > data[swapIndex]))) {
+      swapIndex = rightChild;
+    }
+
+    if (swapIndex == index) break;
+
+    std::swap(data[index], data[swapIndex]);
+    index = swapIndex;
+  }
+
+  return top;
 }
 
 template <typename T>
 T Heap<T>::peek() const {
-  // COMPLETE HERE
+  if (!isEmpty()) {
+    return data[0];
+  }
   return T{};
 }
 
 template <typename T>
 size_t Heap<T>::size() const {
-  // COMPLETE HERE
-  return 0;
+  return data.size();
 }
 
 template <typename T>
 bool Heap<T>::isEmpty() const {
-  // COMPLETE HERE
-  return true;
+  return data.empty();
 }
-
 
 template <typename T>
 void Heap<T>::clear() {
-  // COMPLETE HERE
+  data.clear();
 }
 
 #endif
